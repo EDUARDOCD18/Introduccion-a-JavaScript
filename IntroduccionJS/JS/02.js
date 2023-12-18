062550e37c89d3cf14b4820b65d249f533713727
eduardocd18 <javierecd@gmail.com>
2023-12-18 12:38:12 -0400

    Variables con let y const

---
+++ IntroduccionJS/JS/02.js
@@ -0,0 +1,13 @@
+// Variables con let
+
+let producto = "Audifonos Gamer"
+
+let disponible
+disponible = true
+
+let producto1 = "Computadora",
+    dispobible1 = true,
+    categoria = "Computadoras"
+
+let disponible_;
+
---
+++ IntroduccionJS/JS/03.js
@@ -0,0 +1,14 @@
+// Variables con const
+
+const producto = "Audifonos Gamer"
+
+const disponible = false
+// disponible = true
+
+const producto1 = "Computadora",
+    dispobible1 = true,
+    categoria = "Computadoras"
+
+const disponible_ = 2;
+
+ 
