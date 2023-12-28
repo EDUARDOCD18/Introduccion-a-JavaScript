785f51dececeec960c96000392ae1a3032f65fcd
eduardocd18 <javierecd@gmail.com>
2023-12-28 18:00:15 -0400

    Concatenación y Template Strings

---
+++ IntroduccionJS/08.js
@@ -0,0 +1,10 @@
+// 08 - Concatenación y Templates de Strings
+
+const nombre = "Javier",
+  email = "javierecd18@javier";
+
+// Concatencaión Clásica
+console.log(nombre + " " + email);
+
+// Tempalte String o String Literals (más moderno)
+console.log(`Nombre: ${nombre} \n Email: ${email}`);
