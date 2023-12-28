00bcb2502157ccb17f8932b6efe36f3bf9390d4e
eduardocd18 <javierecd@gmail.com>
2023-12-28 17:23:19 -0400

    El Objeto Math en JS

---
+++ IntroduccionJS/JS/06.js
@@ -0,0 +1,41 @@
+// Objeto Math
+
+// Número ramdom menor a 1
+let nRandomMenor = Math.random()
+console.log(nRandomMenor);
+
+// Número PI
+let numeroPI = Math.PI;
+console.log(numeroPI);
+
+// Redondear número
+let redondeo = Math.round(9.6);
+console.log(redondeo);
+
+// Redondear hacia arriba
+let redondeoArriba = Math.ceil(2.1);
+console.log(redondeoArriba);
+
+// Redondear hacia abajo
+let redondeoAbajo = Math.floor(2.1);
+console.log(redondeoAbajo);
+
+// Raíz Cuadrada
+let raiz = Math.sqrt(144);
+console.log(raiz);
+
+// Negativo a Positivo
+let convertirAbs = Math.abs(-200);
+console.log(convertirAbs);
+
+// Mínimo Número de Listado
+let minimo = Math.min(2, 5, 7, 9, 157, 0);
+console.log(minimo);
+
+// Máximo Número de Listado
+let maximo = Math.max(2, 5, 7, 9, 157, 0);
+console.log(maximo);
+
+// Número Random mayor a uno
+let nRandom = Math.floor(Math.random() * 30);
+console.log(nRandom);
