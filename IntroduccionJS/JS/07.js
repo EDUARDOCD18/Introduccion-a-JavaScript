717040a980d35d195aef4086002ea14b1db7bd35
eduardocd18 <javierecd@gmail.com>
2023-12-28 17:46:07 -0400

    Controlar el Orden de las operaciones

---
+++ IntroduccionJS/JS/07.js
@@ -0,0 +1,21 @@
+// Orden de las Operaciones
+
+let resultado = 20 + 30 * 2;
+console.log(resultado);
+
+let resultado2 = (20 + 30) * 2;
+console.log(resultado2);
+
+// Incrementos
+let puntaje = 10;
+console.log(puntaje);
+
+puntaje++;
+console.log(puntaje);
+
+// Drecemento
+puntaje = 10
+puntaje--;
+console.log(puntaje);
+
+
