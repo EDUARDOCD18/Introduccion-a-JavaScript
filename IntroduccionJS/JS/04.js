cf2a6ba9922d4021085806e6906759d0502f7580
eduardocd18 <javierecd@gmail.com>
2023-12-28 07:31:30 -0400

    Strings o Cadenas de Texto

---
+++ IntroduccionJS/JS/04.js
@@ -0,0 +1,9 @@
+// String o Cadenas de Texto
+
+const producto = "Monitor Gamer 20\"";
+const producto2 = String("Ratón");
+const producto3 = new String("Teclado");
+
+console.log(producto);
+console.log(producto2);
+console.log(typeof producto3);
