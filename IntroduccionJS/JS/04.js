a025a41bdab459e833f979d5bdfc881774557635
eduardocd18 <javierecd@gmail.com>
2023-12-28 07:49:07 -0400

    Métodos para los Strings

--- IntroduccionJS/JS/04.js
+++ IntroduccionJS/JS/04.js
@@ -1,9 +1,18 @@
 // String o Cadenas de Texto
 
 const producto = "Monitor Gamer 20\"";
-const producto2 = String("Ratón");
-const producto3 = new String("Teclado");
+const producto2 = String("Ratón gamer de alta calidad!");
+// const producto3 = new String("Teclado");
 
 console.log(producto);
-console.log(producto2);
-console.log(typeof producto3);
+
+// length
+console.log(producto2.length);
+
+// indexOf (retorna posición)
+console.log(producto2.indexOf("alta"));
+
+// Includes (retorna Booleanos)
+console.log(producto2.includes("alta"));
+
+// console.log(typeof producto3);
