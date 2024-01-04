823f5c88a89f7f7a187234d62379105fe388e62d
eduardocd18 <javierecd@gmail.com>
2024-01-03 21:02:46 -0400

    Booleans en JS

---
+++ IntroduccionJS/09.js
@@ -0,0 +1,14 @@
+// Boolenanos 
+const boolean1 = true
+const boolean2 = false
+
+console.log(boolean1);
+console.log(typeof boolean1);
+
+console.log(boolean2);
+console.log(typeof boolean2);
+
+// Boleanos por medio de Constructor
+
+const boolean3 = new Boolean(true)
+console.log(boolean3);
