a1343e694d22f2549a0fca20608e0fba121cd190
eduardocd18 <javierecd@gmail.com>
2024-01-04 16:28:43 -0400

    Obejtos en JS

---
+++ IntroduccionJS/10.js
@@ -0,0 +1,16 @@
+// Obejtos
+
+// Sintaxis de un Objeto
+const producto = {
+  nombreProducto: 'Monitor de 20"',
+  precio: 300,
+  disponible: true,
+};
+
+console.log(producto);
+
+// Sintaxis de .
+console.log(producto.precio);
+
+// Otra manera de acceder a las propiedades de los objetos
+console.log(producto["nombreProducto"]);
