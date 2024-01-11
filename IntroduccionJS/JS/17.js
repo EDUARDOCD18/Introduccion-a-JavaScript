3345c95b82de5afd17f5c9664326e8a3e0488b36
eduardocd18 <javierecd@gmail.com>
2024-01-11 11:04:00 -0400

    Diferencias entre Métodos y Funciones

---
+++ IntroduccionJS/JS/17.js
@@ -0,0 +1,10 @@
+// 123 - Diferencias entre Métodos y Funciones
+/* Varía dependiendo del contexto en el que se esté utilizando */
+
+const numero1 = 20
+const numero2 = "20"
+
+console.log(parseInt(numero2)); // Función
+console.log(numero1.toString()); // Método
+
+/* lo que sigue después de un . es método */
