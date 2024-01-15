1bde98f3989b0a3ffa4800803e5263427cb20d2c
eduardocd18 <javierecd@gmail.com>
2024-01-15 11:11:27 -0400

    While y Do While

--- IntroduccionJS/JS/24.js
+++ IntroduccionJS/JS/24.js
@@ -1,17 +1,19 @@
 // 130 - For Loops
 "use stric";
 
-// for (let i = 0; i <= 10; i++) {
-//   console.log(i);
-// }
+console.log("FOR LOOP");
 
-// for (let i = 1; i <= 10; i++) {
-//   if (i % 2 === 0) {
-//     console.log(`${i} es par.`);
-//   } else {
-//     console.log(`${i} es impar.`);
-//   }
-// }
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    console.log(`${i} es par.`);
+  } else {
+    console.log(`${i} es impar.`);
+  }
+}
 
 const carrito = [
   { nombre: 'Monitor Gamer de 20"', precio: 450 },
@@ -26,3 +28,30 @@ const carrito = [
 for (let i = 0; i < carrito.length; i++) {
   console.log(carrito[i].precio);
 }
+
+// While Loop
+console.log("\nWHILE LOOP");
+let i = 0;
+
+while (i <= 10) {
+  if (i % 2 == 0) {
+    console.log(`${i} es par.`);
+  } else {
+    console.log(`${i} es impar.`);
+  }
+  i++;
+}
+
+while (i < carrito.length) {
+  console.log(carrito[i].nombre);
+  i++;
+}
+
+// do while loop
+console.log("\nDO WHILE LOOP");
+let i1 = 0;
+
+do {
+  console.log(i1);
+  i1++;
+} while (i1 < 10);
