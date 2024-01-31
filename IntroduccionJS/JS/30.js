8f8fb88589e33456a1ea465b6be880c6f4c7de49
eduardocd18 <javierecd@gmail.com>
2024-01-31 14:25:27 -0400

    138 - Try-catch en JS

---
+++ IntroduccionJS/JS/30.js
@@ -0,0 +1,14 @@
+/* 138 - try cacth */
+
+const numero1 = 20, numero3 = 30
+
+console.log(numero1);
+
+try {
+    console.log(numero2);
+} catch (error) {
+    console.log(error);
+
+}
+
+console.log(numero3);
