59ea3e59e6f219dd2c844e21e5786288a939e66d
eduardocd18 <javierecd@gmail.com>
2024-02-06 16:49:38 -0400

    143 - Fetch API con async/await

--- IntroduccionJS/JS/33.js
+++ IntroduccionJS/JS/33.js
@@ -1,18 +1,21 @@
 /* 143 - Fetch API */
 
-function obtenerEmpleados() {
+async function obtenerEmpleados() {
     const archivo = 'empleados.json';
 
-    fetch(archivo)
+    /* fetch(archivo)
         .then(resultado => resultado.json())
         .then(datos => {
             const { empleados } = datos
             console.log(empleados);
 
-           /*  empleados.forEach(empleado => {
+            empleados.forEach(empleado => {
                 console.log(empleado);
-            }); */
-        })
+            });
+        }) */
+    const resultado = await fetch(archivo)
+    const datos = await resultado.json()
+    console.log(datos);
 }
 
 obtenerEmpleados();
